922398bba9a83fd78545af720231644f7711698c
Srijato Bhattacharyya <srijato@tamu.edu>
2022-11-13 18:29:37 +0530

    Wrote documentation for input parameters of the function.

---
+++ R/MCMCmixtureGaussian.R
@@ -0,0 +1,21 @@
+
+
+#' MCMC for Bimodal Mixtureof 2 Univariate Distributions
+#'
+#' @param mean1 mean of 1st univariate gaussian distribution
+#' @param mean2 mean of 2nd univariate gaussian distribution
+#' @param var1  variance of 1st univariate gaussian distribution
+#' @param var2  variance of 2nd univariate gaussian distribution
+#' @param nsim  number of values to be simulated from the mixture distribution
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, nsim){
+
+  return(0)
+
+}
+
