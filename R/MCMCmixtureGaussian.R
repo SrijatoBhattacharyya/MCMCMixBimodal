f522b3cbd9be5c741b4887dba8ed9d440f4fc3c0
Srijato Bhattacharyya <srijato@tamu.edu>
2022-11-13 18:56:44 -0600

    Updated MCMCMixtureGaussian Documentation with description

--- R/MCMCmixtureGaussian.R
+++ R/MCMCmixtureGaussian.R
@@ -1,6 +1,9 @@
-
-
-#' MCMC for Bimodal Mixtureof 2 Univariate Distributions
+#' MCMC mixture Gaussian
+#'
+#' MCMCmixtureGaussian function simulate
+#'random variables from a Univariate Gaussian Mixture Distribution. The means and variances of the 2
+#'Gaussian distributions and the mixing parameter that form the target mixture distribution will be taken
+#'as input from the user, along with the number of values to be simulated.
 #'
 #' @param mean1 mean of 1st univariate gaussian distribution
 #' @param mean2 mean of 2nd univariate gaussian distribution
@@ -8,10 +11,11 @@
 #' @param var2  variance of 2nd univariate gaussian distribution
 #' @param nsim  number of values to be simulated from the mixture distribution
 #'
-#' @return
+#' @return vector of simulated values from the target distribution
 #' @export
 #'
 #' @examples
+#' MCMCmixtureGaussian(mean1, mean2, var1, var2, nsim)
 #'
 MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, nsim){
 
