3c1d214632e7f6be80c1f7a8609754c72cd41d77
Srijato Bhattacharyya <srijato@tamu.edu>
2022-11-18 00:22:35 -0600

    Created helper functions for my package.

--- R/MCMCmixtureGaussian.R
+++ R/MCMCmixtureGaussian.R
@@ -17,66 +17,84 @@
 #' @examples
 #' MCMCmixtureGaussian(mean1, mean2, var1, var2, alpha, nsim)
 #'
-MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, alpha, nsim){
+#'
+#'
+#'
 
-  #made the function for bimodal density
-  f <- function(x)
+
+#---------------------------------------------#
+# function for target bimodal density
+#---------------------------------------------#
+
+
+target_density <- function(x)
+{
+  W= alpha * dnorm(x, mean1, var1)
+  V=(1- alpha)*dnorm(x, mean2 ,var2)
+  return(W+V)
+}
+
+#-----------------------------------------------#
+#function to generate Markov chain
+#-----------------------------------------------#
+
+
+Chain <- function(t, mean1, mean2, var1, var2, Nsim)
+{
+  mean11 = min(mean1, mean2)
+  mean22 = max(mean1, mean2)
+
+  X[1]=t                  #Initialized the chain
+
+  for(i in 2:Nsim)
   {
-    W= alpha * dnorm(x, mean1, var1)
-    V=(1- alpha)*dnorm(x, mean2 ,var2)
-    return(W+V)
+    # Code for Proposal Density
+    W=rnorm(1,( alpha * mean22) + ((1 - alpha) * X[i-1]), 5)
+    V=rnorm(1,(alpha * mean11) + ((1 - alpha) * X[i-1]), 5)
+
+    # Adjusting the proposal density based on the location of the previously iterated value
+    if(abs(X[i-1] - mean11) < abs(X[i-1] - mean22))
+    {
+      Y=W
+    }
+    else
+    {
+      Y=V
+    }
+
+
+    rho = target_density(Y) / target_density(X[i-1])    # defined the rate parameter for Metropolis Hastings Algorithm
+
+    # stored the next iterated value of the chain
+    if(runif(1) < rho)
+    {
+      X[i] = Y
+    }
+    else
+    {
+      X[i] = X[i-1]
+    }
   }
-  # curve(f(x), -10, 30)
-  # Nsim=500
-  n=1000                              # declared number of replications of the chain
-  X=rep(0, times=Nsim)                #initialized vector to store chain values
+  return(X)
+}
+
+
+#--------------------------------------------#
+#function to return values from simultanious running of the chain and generate histogram from the data
+
+MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, alpha, Nsim){
 
   mean11 = min(mean1, mean2)
   mean22 = max(mean1, mean2)
 
-  #function to generate Markov chain
+  n=1000                              # declared number of replications of the chain
+  X=rep(0, times=Nsim)                #initialized vector to store chain values
 
-  h <- function(t)
-  {
-
-    X[1]=t                  #Initialized the chain
-
-    for(i in 2:Nsim)
-    {
-      # Code for Proposal Density
-      W=rnorm(1,( alpha * mean22) + ((1 - alpha) * X[i-1]), 5)
-      V=rnorm(1,(alpha * mean11) + ((1 - alpha) * X[i-1]), 5)
-
-      # Adjusting the proposal density based on the location of the previously iterated value
-      if(abs(X[i-1] - mean11) < abs(X[i-1] - mean22))
-      {
-        Y=W
-      }
-      else
-      {
-        Y=V
-      }
-
-
-      rho = f(Y) / f(X[i-1])    # defined the rate parameter for Metropolis Hastings Algorithm
-
-      # stored the next iterated value of the chain
-      if(runif(1) < rho)
-      {
-        X[i] = Y
-      }
-      else
-      {
-        X[i] = X[i-1]
-      }
-    }
-    return(X)
-  }
-  Z=do.call("rbind",replicate(n, h(mean1), simplify = F))
+  Z=do.call("rbind",replicate(n, chain(mean11, mean1, mean2, var1, var2, Nsim), simplify = F))
   R=Z[,500]
 
   #histogram of generated values along with the density curve
   hist(R, breaks=70, prob=T)
-  curve(f(x), mean11 - 3*var1, mean22 + 3*var2, add=T)
+  curve(target_density(x), mean11 - 3*var1, mean22 + 3*var2, add=T)
 }
 
