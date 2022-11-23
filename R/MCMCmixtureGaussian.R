f909b85cc05f820348b9f21aed1fee2938b9a1ca
Srijato Bhattacharyya <srijato@Srijatos-MacBook-Pro.local>
2022-11-23 03:19:17 -0600

    Debugged MCMCMixture Gaussian Function and updated example

--- R/MCMCmixtureGaussian.R
+++ R/MCMCmixtureGaussian.R
@@ -1,36 +1,12 @@
-#' MCMC mixture Gaussian
-#'
-#' MCMCmixtureGaussian function simulate
-#'random variables from a Univariate Gaussian Mixture Distribution. The means and variances of the 2
-#'Gaussian distributions and the mixing parameter that form the target mixture distribution will be taken
-#'as input from the user, along with the number of values to be simulated.
-#'
-#' @param mean1 mean of 1st univariate gaussian distribution
-#' @param mean2 mean of 2nd univariate gaussian distribution
-#' @param var1  variance of 1st univariate gaussian distribution
-#' @param var2  variance of 2nd univariate gaussian distribution
-#' @param nsim  number of values to be simulated from the mixture distribution
-#'
-#' @return vector of simulated values from the target distribution
-#' @export
-#'
-#' @examples
-#' MCMCmixtureGaussian(mean1, mean2, var1, var2, alpha, nsim)
-#'
-#'
-#'
-#'
-
-
 #---------------------------------------------#
 # function for target bimodal density
 #---------------------------------------------#
 
 
-target_density <- function(x)
+target_density <- function(x, mean1, mean2, var1, var2, alpha)
 {
-  W= alpha * dnorm(x, mean1, var1)
-  V=(1- alpha)*dnorm(x, mean2 ,var2)
+  W= alpha * stats :: dnorm(x, mean1, var1)
+  V=(1- alpha) * stats :: dnorm(x, mean2 ,var2)
   return(W+V)
 }
 
@@ -39,8 +15,9 @@ target_density <- function(x)
 #-----------------------------------------------#
 
 
-Chain <- function(t, mean1, mean2, var1, var2, Nsim)
+chain <- function(t, mean1, mean2, var1, var2, alpha, Nsim)
 {
+  X = rep(0, Nsim)
   mean11 = min(mean1, mean2)
   mean22 = max(mean1, mean2)
 
@@ -49,8 +26,8 @@ Chain <- function(t, mean1, mean2, var1, var2, Nsim)
   for(i in 2:Nsim)
   {
     # Code for Proposal Density
-    W=rnorm(1,( alpha * mean22) + ((1 - alpha) * X[i-1]), 5)
-    V=rnorm(1,(alpha * mean11) + ((1 - alpha) * X[i-1]), 5)
+    W = stats :: rnorm(1,( alpha * mean22) + ((1 - alpha) * X[i-1]), 5)
+    V= stats :: rnorm(1,(alpha * mean11) + ((1 - alpha) * X[i-1]), 5)
 
     # Adjusting the proposal density based on the location of the previously iterated value
     if(abs(X[i-1] - mean11) < abs(X[i-1] - mean22))
@@ -63,10 +40,10 @@ Chain <- function(t, mean1, mean2, var1, var2, Nsim)
     }
 
 
-    rho = target_density(Y) / target_density(X[i-1])    # defined the rate parameter for Metropolis Hastings Algorithm
+    rho = target_density(Y, mean1, mean2, var1, var2, alpha) / target_density(X[i-1], mean1, mean2, var1, var2, alpha)    # defined the rate parameter for Metropolis Hastings Algorithm
 
     # stored the next iterated value of the chain
-    if(runif(1) < rho)
+    if(stats :: runif(1) < rho)
     {
       X[i] = Y
     }
@@ -79,8 +56,35 @@ Chain <- function(t, mean1, mean2, var1, var2, Nsim)
 }
 
 
+
+
+
 #--------------------------------------------#
 #function to return values from simultanious running of the chain and generate histogram from the data
+
+#' MCMC mixture Gaussian
+#'
+#' MCMCmixtureGaussian function simulate
+#' random variables from a Univariate Gaussian Mixture Distribution. The means and variances of the 2
+#' Gaussian distributions and the mixing parameter that form the target mixture distribution will be taken
+#' as input from the user, along with the number of values to be simulated.
+#'
+#' @param mean1 mean of 1st univariate gaussian distribution
+#' @param mean2 mean of 2nd univariate gaussian distribution
+#' @param var1  variance of 1st univariate gaussian distribution
+#' @param var2  variance of 2nd univariate gaussian distribution
+#' @param alpha mixture proportion of the normal density with smaller mean
+#' @param Nsim  number of values to be simulated from the mixture distribution
+#'
+#' @return vector of simulated values from the target distribution
+#' @export
+#'
+#' @examples
+#' MCMCmixtureGaussian(1, 10, 2, 3, 0.3, 1000)
+#'
+#'
+#'
+#'
 
 MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, alpha, Nsim){
 
@@ -88,13 +92,13 @@ MCMCmixtureGaussian <- function(mean1, mean2, var1, var2, alpha, Nsim){
   mean22 = max(mean1, mean2)
 
   n=1000                              # declared number of replications of the chain
-  X=rep(0, times=Nsim)                #initialized vector to store chain values
+  # X=rep(0, times=Nsim)                #initialized vector to store chain values
 
-  Z=do.call("rbind",replicate(n, chain(mean11, mean1, mean2, var1, var2, Nsim), simplify = F))
-  R=Z[,500]
+  Z=do.call("rbind",replicate(n, chain(mean11, mean1, mean2, var1, var2, alpha, Nsim), simplify = F))
+  R=Z[, 500]
 
   #histogram of generated values along with the density curve
-  hist(R, breaks=70, prob=T)
-  curve(target_density(x), mean11 - 3*var1, mean22 + 3*var2, add=T)
+  graphics :: hist(R, breaks=70, prob=T)
+  # graphics :: curve(target_density(x, mean1, mean2, var1, var2, alpha), mean11 - 3*var1, mean22 + 3*var2, add=T)
 }
 
