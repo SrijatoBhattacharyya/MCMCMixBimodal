e8d83d1f3a76e246f25e7fc188abc8ec8fe29a2b
Srijato Bhattacharyya <srijato@tamu.edu>
2022-12-02 21:14:38 -0600

    MCMCrateGaussian function implementation

--- R/MCMCrateGaussian.R
+++ R/MCMCrateGaussian.R
@@ -1,3 +1,8 @@
+
+# source(here::here("R", "MCMCmixtureGaussian.R"))
+
+
+
 #' MCMC rate Gaussian
 #'
 #'
@@ -10,15 +15,47 @@
 #' @param mean2 mean of 2nd univariate gaussian distribution
 #' @param var1  variance of 1st univariate gaussian distribution
 #' @param var2  variance of 2nd univariate gaussian distribution
-#' @param nsim  number of values to be simulated from the mixture distribution
+#' @param alpha value of mixing parameter
+#' @param Nsim  number of values to be simulated from the mixture distribution
 #'
 #' @return plot of supremum of the absolute
 #' difference between the empirical CDF of the 500 generated values of the chain and the actual CDF of the
 #' target distribution at each iteration.
 #' @export
 #'
-#' @examples MCMCrateGaussian(mean1, mean2, var1, var2, nsim)
+#' @examples
+#' MCMCrateGaussian(mean1 = 20, mean2 = 0, var1= 1, var2 = 2, alpha= 0.5, Nsim= 1000)
 #'
-MCMCrateGaussian <- function(mean1, mean2, var1, var2, nsim) {
-  return(0)
+
+
+
+MCMCrateGaussian <- function(mean1, mean2, var1, var2, alpha, Nsim) {
+  n = 1000
+  S=c()
+
+  Z <- do.call("rbind", replicate(n, chain(mean1, mean1, mean2, var1, var2, alpha, Nsim), simplify = F))
+
+  # Z=do.call("rbind",replicate(n, chain( t = mean1, mean1 = mean1, mean2 = mean2, var1 = var1, var2 = var2, alpha = alpha, Nsim = Nsim), simplify = F))
+  for(j in 1: 50)
+  {
+    P=Z[,j]
+    V=sort(P)
+    E=c()
+    for(i in 1:n)
+    {
+      E[i]=i/n
+    }
+    O=c()
+    for(i in 1:n)
+    {
+      x = V[i]
+      O[i]=abs(E[i]- ((alpha * stats::pnorm(x, mean1, var1))+((1 - alpha) * stats::pnorm(x, mean2, var2))) )
+    }
+    M=max(O)
+    S[j]=M
+  }
+
+  plot(S)
+
 }
+
