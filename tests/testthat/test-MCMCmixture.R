8b7b8a355979970625e471da4bda64cecb860dee
Srijato Bhattacharyya <srijato@tamu.edu>
2022-12-07 18:25:31 -0600

    Updated tests for the functions

--- tests/testthat/test-MCMCmixture.R
+++ tests/testthat/test-MCMCmixture.R
@@ -1,3 +1,56 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat::test_that("MCMCmixture test cases", {
+
+  #expecting a value
+  testthat::expect_length(target_density(x = 8, m1 = 2, m2 = 7, s1 = 2, s2 = 1, alpha = 0.1, density = "Normal"),1)
+
+  #expecting warning message
+  testthat::expect_error(target_density(x = 8, m1 = 2, m2 = 7, s1 = -2, s2 = 1, alpha = 0.1, density = "Normal"))
+
+  #expecting warning message
+  testthat::expect_error(target_density(x = 8, m1 = 2, m2 = 7, s1 = 2, s2 = 1, alpha = 10, density = "Normal"))
+
+  #expecting warning message
+  testthat::expect_error(target_density(x = 8, m1 = 2, m2 = 7, s1 = 2, s2 = 1, alpha = -5, density = "Normal"))
+
+
+  # expecting a vector of length 100
+  testthat::expect_length(chain(2, 1, 10, 2, 3, 0.3, 100), 100)
+
+  # expecting a vector of length 1000
+  testthat::expect_length(chain(2, 1, 10, 2, 3, 0.3, 1000), 1000)
+
+  #expecting warning message
+  testthat::expect_error(chain(2, 1, 10, 2, 3, 3, 1000))
+
+  #expecting warning message
+  testthat::expect_error(chain(2, 1, 10, -2, 3, 0.3, 1000))
+
+
+  # expecting warning message
+  testthat::expect_error(MCMCmixture(1, 10, 2, 3, 3, -1000, density = "Normal"))
+
+
+
+  # expecting a vector of length 1000
+  testthat::expect_length(MCMCmixture(1, 10, 2, 3, 0.3, 1000, density = "Normal"), 1000)
+
+  # expecting a vector of length 1000
+  testthat::expect_length(MCMCmixture(1, 10, 2, 2, 0.5, 1000, density = "Cauchy"), 1000)
+
+
+  # expecting warning message
+  testthat::expect_error(MCMCmixture(1, 10, 2, 3, 3, 1000, density = "Normal"))
+
+  # expecting warning message
+  testthat::expect_error(MCMCmixture(1, 10, 2, 3, 0.3, 10, density = "Normal"))
+
+  # expecting warning message
+  testthat::expect_error(MCMCmixture(1, 10, -2, 3, 3, 1000, density = "Normal"))
+
+  # expecting warning message
+  testthat::expect_error(MCMCmixture(1, 10, 2, 3, 3, 100.5, density = "Normal"))
+
+
+
+
 })
--- tests/testthat/test-MCMCrate.R
+++ tests/testthat/test-MCMCrate.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat::test_that("MCMCrate works", {
+
+  #expect warning message
+  testthat::expect_error(MCMCrate(m1 = 20, m2 = 0, s1 = 1, s2 = 2, alpha = 0.5, Nsim = 10))
+
+  #expect warning message
+  testthat::expect_error(MCMCrate(m1 = 20, m2 = 0, s1 = 1, s2 = -2, alpha = 0.5, Nsim = 1000))
+
+  #expect warning message
+  testthat::expect_error(MCMCrate(m1 = 20, m2 = 0, s1 = 1, s2 = 2, alpha = 5, Nsim = 1000))
+
+  #expect warning message
+  testthat::expect_error(MCMCrate(m1 = 20, m2 = 0, s1 = 1, s2 = 2, alpha = -3, Nsim = 1000))
+
+  #expect a vector of length 100
+  testthat::expect_length(MCMCrate(m1 = 20, m2 = 0, s1 = 1, s2 = 2, alpha = 0.5, Nsim = 100), 100)
+
+
 })
